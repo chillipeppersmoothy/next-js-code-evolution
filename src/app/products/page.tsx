dfd693a163924c7a07a3df7c2fd93e54d66b3d78
Aditya Shenoy K <adityashenoy11@gmail.com>
2025-03-22 08:38:17 +0530

    first commit

---
+++ src/app/products/page.tsx
@@ -0,0 +1,10 @@
+export default function Products() {
+  return (
+    <div>
+      <h1>Products Page</h1>
+      <h3>Products 1</h3>
+      <h3>Products 2</h3>
+      <h3>Products 3</h3>
+    </div>
+  );
+}
